ba3d943299f905193a8756ae7a1553513ee492d4
Eniel <eniel160990@gmail.com>
2020-10-27 23:04:07 -0500

    Client: Limit supervise-user feature only for company admins

--- client/src/app/admin/panel/users/admin-panel-view-user.js
+++ client/src/app/admin/panel/users/admin-panel-view-user.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {connect}  from 'react-redux';
+import {connect} from 'react-redux';
 import history from 'lib-app/history';
 
 import i18n from 'lib-app/i18n';
@@ -37,7 +37,8 @@ class AdminPanelViewUser extends React.Component {
     render() {
         return (
             <div className="admin-panel-view-user">
-                <Header title={i18n('USER_VIEW_TITLE', {userId: this.props.params.userId})} description={i18n('USER_VIEW_DESCRIPTION')} />
+                <Header title={i18n('USER_VIEW_TITLE', {userId: this.props.params.userId})}
+                        description={i18n('USER_VIEW_DESCRIPTION')}/>
                 {(this.state.invalid) ? this.renderInvalid() : this.renderUserInfo()}
             </div>
         );
@@ -84,35 +85,21 @@ class AdminPanelViewUser extends React.Component {
                             type={this.state.disabled ? 'tertiary' : 'primary'}>
                             {i18n(this.state.disabled ? 'ENABLE_USER' : 'DISABLE_USER')}
                         </Button>
-                        <Button className="admin-panel-view-user__action-button" onClick={this.onDeleteAndBanClick.bind(this)} size="medium">
+                        <Button className="admin-panel-view-user__action-button"
+                                onClick={this.onDeleteAndBanClick.bind(this)} size="medium">
                             {i18n('DELETE_AND_BAN')}
                         </Button>
-                        <Button className="admin-panel-view-user__action-button" onClick={this.onDeleteClick.bind(this)} size="medium">
+                        <Button className="admin-panel-view-user__action-button" onClick={this.onDeleteClick.bind(this)}
+                                size="medium">
                             {i18n('DELETE')}
                         </Button>
                     </div>
                 </div>
-                <span className="separator" />
-                <div className="admin-panel-view-user">
-                    <div className="admin-panel-view-user__supervised-users-header">{i18n('SUPERVISED_USER')}</div>
-                    <div className="admin-panel-view-user__supervised-users-content">
-                        <Autocomplete
-                            onChange={this.onChangeValues.bind(this)}
-                            getItemListFromQuery={this.searchUsers.bind(this)}
-                            values={this.transformUserListToAutocomplete()}
-                        />
-                        <Button
-                            disabled = {this.state.loading}
-                            className="admin-panel-view-user__submit-button"
-                            onClick={this.onClickSupervisorUserButton.bind(this)}
-                            size="medium"
-                        >
-                            {i18n('UPDATE')}
-                        </Button>
-                    </div>
-                    {this.renderSupervisedUserMessage()}
-                </div>
-                <span className="separator" />
+                <span className="separator"/>
+
+                {this.state.isCompanyAdmin && this.renderSupervisedView()}
+                {this.state.isCompanyAdmin && <span className="separator"/>}
+
                 <div className="admin-panel-view-user__tickets">
                     <div className="admin-panel-view-user__tickets-title">{i18n('TICKETS')}</div>
                     <TicketList {...this.getTicketListProps()}/>
@@ -121,33 +108,57 @@ class AdminPanelViewUser extends React.Component {
         );
     }
 
-    renderSupervisedUserMessage(){
-        if(this.state.message) {
-            if(this.state.message != 'success'){
-                return(
+    renderSupervisedUserMessage() {
+        if (this.state.message) {
+            if (this.state.message != 'success') {
+                return (
                     <div className="admin-panel-view-user__supervised-users-message">
-                            <Message type="error">{i18n(this.state.message)}</Message>
+                        <Message type="error">{i18n(this.state.message)}</Message>
                     </div>
                 )
-            }else{
-                return(
-                    <div className= "admin-panel-view-user__supervised-users-message">
-                            <Message type="success">{i18n('SUPERVISED_USERS_UPDATED')}</Message>
+            } else {
+                return (
+                    <div className="admin-panel-view-user__supervised-users-message">
+                        <Message type="success">{i18n('SUPERVISED_USERS_UPDATED')}</Message>
                     </div>
                 )
             }
         }
     }
 
-    onClickSupervisorUserButton(){
+    renderSupervisedView() {
+        return (
+            <div className="admin-panel-view-user">
+                <div className="admin-panel-view-user__supervised-users-header">{i18n('SUPERVISED_USER')}</div>
+                <div className="admin-panel-view-user__supervised-users-content">
+                    <Autocomplete
+                        onChange={this.onChangeValues.bind(this)}
+                        getItemListFromQuery={this.searchUsers.bind(this)}
+                        values={this.transformUserListToAutocomplete()}
+                    />
+                    <Button
+                        disabled={this.state.loading}
+                        className="admin-panel-view-user__submit-button"
+                        onClick={this.onClickSupervisorUserButton.bind(this)}
+                        size="medium"
+                    >
+                        {i18n('UPDATE')}
+                    </Button>
+                </div>
+                {this.renderSupervisedUserMessage()}
+            </div>
+        )
+    }
+
+    onClickSupervisorUserButton() {
         this.setState({
             loading: true
         });
-        
+
         const userIdList = this.state.userList.map((item) => {
             return item.id;
         });
-        
+
         API.call({
             path: '/user/edit-supervised-list',
             data: {
@@ -174,7 +185,9 @@ class AdminPanelViewUser extends React.Component {
     }
 
     searchUsers(query, blacklist = []) {
-        blacklist = blacklist.map(item => {return {isStaff: item.isStaff, id: item.id}});
+        blacklist = blacklist.map(item => {
+            return {isStaff: item.isStaff, id: item.id}
+        });
 
         return API.call({
             path: '/ticket/search-authors',
@@ -190,20 +203,21 @@ class AdminPanelViewUser extends React.Component {
                 return {
                     name: author.name,
                     color: "gray",
-                    id: author.id*1,
+                    id: author.id * 1,
                     content: <div>{author.name}</div>,
                     isStaff: false
-                }});
+                }
+            });
         }).catch((r) => {
             console.log(r);
         });
     }
 
     transformUserListToAutocomplete() {
-        return(
+        return (
             this.state.userList.map((user) => {
                 return ({
-                    id: user.id*1,
+                    id: user.id * 1,
                     name: user.name,
                     content: <div>{user.name}</div>,
                     color: 'grey',
@@ -215,13 +229,13 @@ class AdminPanelViewUser extends React.Component {
 
     renderNotVerified() {
         return (
-            <InfoTooltip className="admin-panel-view-user__unverified" type="warning" text={i18n('UNVERIFIED_EMAIL')} />
+            <InfoTooltip className="admin-panel-view-user__unverified" type="warning" text={i18n('UNVERIFIED_EMAIL')}/>
         );
     }
 
     renderDisabled() {
         return (
-            <InfoTooltip className="admin-panel-view-user__unverified" type="warning" text={i18n('USER_DISABLED')} />
+            <InfoTooltip className="admin-panel-view-user__unverified" type="warning" text={i18n('USER_DISABLED')}/>
         );
     }
 
@@ -251,6 +265,7 @@ class AdminPanelViewUser extends React.Component {
             name: result.data.name,
             email: result.data.email,
             company: result.data.company,
+            isCompanyAdmin: result.data.isCompanyAdmin,
             verified: result.data.verified,
             tickets: result.data.tickets,
             disabled: result.data.disabled,
