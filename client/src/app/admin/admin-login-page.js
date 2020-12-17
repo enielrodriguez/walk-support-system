c942ef90332e93172824c45776cd118752fe42b4
Eniel <eniel160990@gmail.com>
2020-12-17 01:11:59 -0500

    Replace OpenSupports title by Support System

--- client/src/app/admin/admin-login-page.js
+++ client/src/app/admin/admin-login-page.js
@@ -50,7 +50,7 @@ class AdminLoginPage extends React.Component {
             <div>
                 <Widget className="admin-login-page__content">
                     <div className="admin-login-page__image"><img width="100%" src={API.getURL() + '/images/logo.png'}
-                                                                  alt="OpenSupports Admin Panel"/></div>
+                                                                  alt="Support Admin Panel"/></div>
                     <div className="admin-login-page__login-form-container">
                         <Form {...this.getLoginFormProps()}>
                             <div className="admin-login-page__login-form-container__login-form__fields">
